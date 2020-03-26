6dc251bfd384eeedf131b83a83ca1c8b9a78bc1e
BeatrizMata <32656614+BeatrizMata@users.noreply.github.com>
2020-03-25 21:02:59 -0600

    a
    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CasesComponent} from './cases/cases.component';
+import {CasesDetailsComponent} from './cases-details/cases-details.component';
+import {CasesStatComponent} from './cases-stat/cases-stat.component';
+import {EditCasesComponent} from './edit-cases/edit-cases.component';
+import {AddCasesComponent} from './add-cases/add-cases.component';
 
+const routes: Routes = [
+  {
+    path: 'cases',
+    component: CasesComponent,
+    data: {title: 'Lista de casos'}
+  },
+  {
+    path: 'cases-details/:id',
+    component: CasesDetailsComponent,
+    data: {title: 'Cases Details'}
+  },
+  {
+    path: 'cases-stat',
+    component: CasesStatComponent,
+    data: {title: 'Estadísticas'}
+  },
+  {
+    path: 'add-cases',
+    component: AddCasesComponent,
+    data: {title: 'Agregar casos'}
+  },
+  {
+    path: 'edit-cases/:id',
+    component: EditCasesComponent,
+    data: {title: 'Modificando casos'}
+  },
+  {
+    path: '',
+    redirectTo: '/cases',
+    pathMatch: 'full'
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
